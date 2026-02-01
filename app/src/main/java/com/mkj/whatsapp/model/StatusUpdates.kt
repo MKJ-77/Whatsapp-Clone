ac5f1af598fe2e1c43f19ca9be4d641651af7a8c
MKJ-77 <mkjmp77@gmail.com>
2026-02-01 17:40:11 +0530

    Update Screen UI done

--- app/src/main/java/com/mkj/whatsapp/model/StatusUpdates.kt
+++ app/src/main/java/com/mkj/whatsapp/model/StatusUpdates.kt
@@ -3,5 +3,7 @@ package com.mkj.whatsapp.model
 data class StatusUpdate(
     val name: String,
     val image: Int,
-    val time: String
+    val time: String,
+    val statusCount: Int,
+    val isViewed: Boolean
 )
